ea3726ddc099f6c73ac4205d7fbe3152c93e4cf2
gmfractal <fractalnow@gmail.com>
2023-04-15 22:56:29 -0400

    update exports

--- index.ts
+++ index.ts
@@ -2,4 +2,7 @@ export * from "./observables/observable";
 export * from "./operators/map-operator";
 export * from "./operators/filter-operator";
 export * from "./operators/tap-operator";
+export * from "./operators/take-operator";
+export * from "./operators/skip-operator";
+export * from "./operators/start-with-operator";
 export * from "./types/observables-library-types";
